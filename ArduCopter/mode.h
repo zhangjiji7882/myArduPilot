2a62ed4d81349d9635adcd023d18b9cd8dc97084
zhangjiji7882 <38725749+zhangjiji7882@users.noreply.github.com>
2025-06-09 20:29:36 +0800

    update draw star
    update draw star

--- ArduCopter/mode.h
+++ ArduCopter/mode.h
@@ -1157,7 +1157,7 @@ public:
 
     // inherit constructor
     using Mode::Mode;
-    Number mode_number() const override { return Number::GUIDED; }
+    Number mode_number() const override { return Number::DRAWSTAR; }
 
     bool init(bool ignore_checks) override;
     void run() override;
@@ -1166,120 +1166,25 @@ public:
     bool has_manual_throttle() const override { return false; }
     bool allows_arming(AP_Arming::Method method) const override;
     bool is_autopilot() const override { return true; }
-    bool has_user_takeoff(bool must_navigate) const override { return true; }
+    bool has_user_takeoff(bool must_navigate) const override { return false; }
     bool in_guided_mode() const override { return true; }
-
-    bool requires_terrain_failsafe() const override { return true; }
-
-    // Sets guided's angular target submode: Using a rotation quaternion, angular velocity, and climbrate or thrust (depends on user option)
-    // attitude_quat: IF zero: ang_vel (angular velocity) must be provided even if all zeroes
-    //                IF non-zero: attitude_control is performed using both the attitude quaternion and angular velocity
-    // ang_vel: angular velocity (rad/s)
-    // climb_rate_cms_or_thrust: represents either the climb_rate (cm/s) or thrust scaled from [0, 1], unitless
-    // use_thrust: IF true: climb_rate_cms_or_thrust represents thrust
-    //             IF false: climb_rate_cms_or_thrust represents climb_rate (cm/s)
-   
-    bool get_wp(Location &loc) const override;
- 
-    // get position, velocity and acceleration targets
-    const Vector3p& get_target_pos() const;
-    const Vector3f& get_target_vel() const;
-    const Vector3f& get_target_accel() const;
-
-    // returns true if GUIDED_OPTIONS param suggests SET_ATTITUDE_TARGET's "thrust" field should be interpreted as thrust instead of climb rate
-
-    bool use_wpnav_for_position_control() const;
-
-    void limit_clear();
-    void limit_init_time_and_pos();
-    void limit_set(uint32_t timeout_ms, float alt_min_cm, float alt_max_cm, float horiz_max_cm);
-    bool limit_check();
-
-    bool is_taking_off() const override;
-    
-
-    // initialises position controller to implement take-off
-    // takeoff_alt_cm is interpreted as alt-above-home (in cm) or alt-above-terrain if a rangefinder is available
-    bool do_user_takeoff_start(float takeoff_alt_cm) override;
-
-    enum class SubMode {
-        TakeOff,
-        WP,
-        Pos,
-        PosVelAccel,
-        VelAccel,
-        Accel,
-        Angle,
-    };
-
-    SubMode submode() const { return guided_mode; }
-
-    void angle_control_start();
-    void angle_control_run();
-
-    // return guided mode timeout in milliseconds. Only used for velocity, acceleration, angle control, and angular rate control
-    uint32_t get_timeout_ms() const;
-
-    bool use_pilot_yaw() const override;
-
-    // pause continue in guided mode
-    bool pause() override;
-    bool resume() override;
-
-    // true if weathervaning is allowed in guided
-#if WEATHERVANE_ENABLED == ENABLED
-    bool allows_weathervaning(void) const override;
-#endif
 
 protected:
 
-    const char *name() const override { return "GUIDED"; }
-    const char *name4() const override { return "GUID"; }
-
-    uint32_t wp_distance() const override;
-    int32_t wp_bearing() const override;
-    float crosstrack_error() const override;
+    const char *name() const override { return "DRAWSTAR"; }
+    const char *name4() const override { return "DRAWSTAR"; }
 
 private:
-
-    // enum for GUID_OPTIONS parameter
-    enum class Options : int32_t {
-        AllowArmingFromTX   = (1U << 0),
-        // this bit is still available, pilot yaw was mapped to bit 2 for symmetry with auto
-        IgnorePilotYaw      = (1U << 2),
-        SetAttitudeTarget_ThrustAsThrust = (1U << 3),
-        DoNotStabilizePositionXY = (1U << 4),
-        DoNotStabilizeVelocityXY = (1U << 5),
-        WPNavUsedForPosControl = (1U << 6),
-        AllowWeatherVaning = (1U << 7)
-    };
 
     Vector3f path[10];
     int path_num;
 
     void generate_path();
 
-    // wp controller
-    void wp_control_start();
-    void wp_control_run();
-
     void pva_control_start();
     void pos_control_start();
-    void accel_control_start();
     void velaccel_control_start();
-    void posvelaccel_control_start();
-    void takeoff_run();
     void pos_control_run();
-    void accel_control_run();
-    void velaccel_control_run();
-    void pause_control_run();
-    void posvelaccel_control_run();
-    void set_yaw_state(bool use_yaw, float yaw_cd, bool use_yaw_rate, float yaw_rate_cds, bool relative_angle);
-
-    // controls which controller is run (pos or vel):
-    SubMode guided_mode = SubMode::TakeOff;
-    bool send_notification;     // used to send one time notification to ground station
-    bool takeoff_complete;      // true once takeoff has completed (used to trigger retracting of landing gear)
 
     // guided mode is paused or not
     bool _paused;
--- ArduCopter/mode_DrawStar.cpp
+++ ArduCopter/mode_DrawStar.cpp
@@ -35,7 +35,6 @@ bool ModeDrawStar::init(bool ignore_checks)
     // velaccel_control_start();
     guided_vel_target_cms.zero();
     guided_accel_target_cmss.zero();
-    send_notification = false;
 
     // clear pause state when entering guided mode
     _paused = false;
@@ -66,9 +65,6 @@ void ModeDrawStar::generate_path()
 // initialise guided mode's position controller
 void ModeDrawStar::pos_control_start()
 {
-    // set to position control mode
-    guided_mode = SubMode::Pos;
-
     // initialise position controller
     pva_control_start();
 }
@@ -88,6 +84,8 @@ void ModeDrawStar::pva_control_start()
     pos_control->init_z_controller();
     pos_control->init_xy_controller();
 
+    wp_nav->set_wp_destination(path[0], false);
+
     // initialise yaw
     auto_yaw.set_mode_to_default(false);
 
@@ -99,52 +97,6 @@ void ModeDrawStar::pva_control_start()
 // should be called at 100hz or more
 void ModeDrawStar::run()
 {
-
-    // run pause control if the vehicle is paused
-    if (_paused) {
-        pause_control_run();
-        return;
-    }
-   /*
-    // call the correct auto controller
-    switch (guided_mode) {
-
-    case SubMode::TakeOff:
-        // run takeoff controller
-        takeoff_run();
-        break;
-
-    case SubMode::WP:
-        // run waypoint controller
-        wp_control_run();
-        if (send_notification && wp_nav->reached_wp_destination()) {
-            send_notification = false;
-            gcs().send_mission_item_reached_message(0);
-        }
-        break;
-
-    case SubMode::Pos:
-        // run position controller
-        pos_control_run();
-        break;
-
-    case SubMode::Accel:
-        accel_control_run();
-        break;
-
-    case SubMode::VelAccel:
-        velaccel_control_run();
-        break;
-
-    case SubMode::PosVelAccel:
-        posvelaccel_control_run();
-        break;
-
-    case SubMode::Angle:
-        angle_control_run();
-        break;
-    }
-        */
     if(path_num < 6){
         if(wp_nav->reached_wp_destination()){
             path_num ++;
@@ -155,127 +107,6 @@ void ModeDrawStar::run()
     pos_control_run();
  }
 
-bool ModeDrawStar::allows_arming(AP_Arming::Method method) const
-{
-    // always allow arming from the ground station or scripting
-    if (AP_Arming::method_is_GCS(method) || method == AP_Arming::Method::SCRIPTING) {
-        return true;
-    }
-
-    // optionally allow arming from the transmitter
-    return (copter.g2.guided_options & (uint32_t)Options::AllowArmingFromTX) != 0;
-};
-
-#if WEATHERVANE_ENABLED == ENABLED
-bool ModeDrawStar::allows_weathervaning() const
-{
-    return (copter.g2.guided_options.get() & (uint32_t)Options::AllowWeatherVaning) != 0;
-}
-#endif
-
-// initialises position controller to implement take-off
-// takeoff_alt_cm is interpreted as alt-above-home (in cm) or alt-above-terrain if a rangefinder is available
-bool ModeDrawStar::do_user_takeoff_start(float takeoff_alt_cm)
-{
-    // calculate target altitude and frame (either alt-above-ekf-origin or alt-above-terrain)
-    int32_t alt_target_cm;
-    bool alt_target_terrain = false;
-    if (wp_nav->rangefinder_used_and_healthy() &&
-        wp_nav->get_terrain_source() == AC_WPNav::TerrainSource::TERRAIN_FROM_RANGEFINDER &&
-        takeoff_alt_cm < copter.rangefinder.max_distance_cm_orient(ROTATION_PITCH_270)) {
-        // can't takeoff downwards
-        if (takeoff_alt_cm <= copter.rangefinder_state.alt_cm) {
-            return false;
-        }
-        // provide target altitude as alt-above-terrain
-        alt_target_cm = takeoff_alt_cm;
-        alt_target_terrain = true;
-    } else {
-        // interpret altitude as alt-above-home
-        Location target_loc = copter.current_loc;
-        target_loc.set_alt_cm(takeoff_alt_cm, Location::AltFrame::ABOVE_HOME);
-
-        // provide target altitude as alt-above-ekf-origin
-        if (!target_loc.get_alt_cm(Location::AltFrame::ABOVE_ORIGIN, alt_target_cm)) {
-            // this should never happen but we reject the command just in case
-            return false;
-        }
-    }
-
-    guided_mode = SubMode::TakeOff;
-
-    // initialise yaw
-    auto_yaw.set_mode(AutoYaw::Mode::HOLD);
-
-    // clear i term when we're taking off
-    pos_control->init_z_controller();
-
-    // initialise alt for WP_NAVALT_MIN and set completion alt
-    auto_takeoff.start(alt_target_cm, alt_target_terrain);
-
-    // record takeoff has not completed
-    takeoff_complete = false;
-
-    return true;
-}
-
-// initialise guided mode's waypoint navigation controller
-void ModeDrawStar::wp_control_start()
-{
-    // set to position control mode
-    guided_mode = SubMode::WP;
-
-    // initialise waypoint and spline controller
-    wp_nav->wp_and_spline_init();
-
-    // initialise wpnav to stopping point
-    Vector3f stopping_point;
-    wp_nav->get_wp_stopping_point(stopping_point);
-    if (!wp_nav->set_wp_destination(stopping_point, false)) {
-        // this should never happen because terrain data is not used
-        INTERNAL_ERROR(AP_InternalError::error_t::flow_of_control);
-    }
-
-    // initialise yaw
-    auto_yaw.set_mode_to_default(false);
-}
-
-// run guided mode's waypoint navigation controller
-void ModeDrawStar::wp_control_run()
-{
-    // if not armed set throttle to zero and exit immediately
-    if (is_disarmed_or_landed()) {
-        // do not spool down tradheli when on the ground with motor interlock enabled
-        make_safe_ground_handling(copter.is_tradheli() && motors->get_interlock());
-        return;
-    }
-
-    // set motors to full range
-    motors->set_desired_spool_state(AP_Motors::DesiredSpoolState::THROTTLE_UNLIMITED);
-
-    // run waypoint controller
-    copter.failsafe_terrain_set_status(wp_nav->update_wpnav());
-
-    // call z-axis position controller (wpnav should have already updated it's alt target)
-    pos_control->update_z_controller();
-
-    // call attitude controller with auto yaw
-    attitude_control->input_thrust_vector_heading(pos_control->get_thrust_vector(), auto_yaw.get_heading());
-}
-
-
-
-
-// initialise guided mode's velocity controller
-void ModeDrawStar::accel_control_start()
-{
-    // set guided_mode to velocity controller
-    guided_mode = SubMode::Accel;
-
-    // initialise position controller
-    pva_control_start();
-}
-
 // initialise guided mode's velocity and acceleration controller
 void ModeDrawStar::velaccel_control_start()
 {
@@ -284,76 +115,6 @@ void ModeDrawStar::velaccel_control_start()
 
     // initialise position controller
     pva_control_start();
-}
-
-// initialise guided mode's position, velocity and acceleration controller
-void ModeDrawStar::posvelaccel_control_start()
-{
-    // set guided_mode to velocity controller
-    guided_mode = SubMode::PosVelAccel;
-
-    // initialise position controller
-    pva_control_start();
-}
-
-bool ModeDrawStar::is_taking_off() const
-{
-    return guided_mode == SubMode::TakeOff && !takeoff_complete;
-}
-
-
-// initialise guided mode's angle controller
-void ModeDrawStar::angle_control_start()
-{
-    // set guided_mode to velocity controller
-    guided_mode = SubMode::Angle;
-
-    // set vertical speed and acceleration limits
-    pos_control->set_max_speed_accel_z(wp_nav->get_default_speed_down(), wp_nav->get_default_speed_up(), wp_nav->get_accel_z());
-    pos_control->set_correction_speed_accel_z(wp_nav->get_default_speed_down(), wp_nav->get_default_speed_up(), wp_nav->get_accel_z());
-
-    // initialise the vertical position controller
-    if (!pos_control->is_active_z()) {
-        pos_control->init_z_controller();
-    }
-
-    // initialise targets
-    guided_angle_state.update_time_ms = millis();
-    guided_angle_state.attitude_quat.from_euler(Vector3f(0.0, 0.0, attitude_control->get_att_target_euler_rad().z));
-    guided_angle_state.ang_vel.zero();
-    guided_angle_state.climb_rate_cms = 0.0f;
-    guided_angle_state.yaw_rate_cds = 0.0f;
-    guided_angle_state.use_yaw_rate = false;
-}
-
-bool ModeDrawStar::get_wp(Location& destination) const
-{
-    switch (guided_mode) {
-    case SubMode::WP:
-        return wp_nav->get_oa_wp_destination(destination);
-    case SubMode::Pos:
-        destination = Location(guided_pos_target_cm.tofloat(), guided_pos_terrain_alt_1 ? Location::AltFrame::ABOVE_TERRAIN : Location::AltFrame::ABOVE_ORIGIN);
-        return true;
-    default:
-        return false;
-    }
-
-    // should never get here but just in case
-    return false;
-}
-
-// takeoff_run - takeoff in guided mode
-//      called by guided_run at 100hz or more
-void ModeDrawStar::takeoff_run()
-{
-    auto_takeoff.run();
-    if (auto_takeoff.complete && !takeoff_complete) {
-        takeoff_complete = true;
-#if AP_LANDINGGEAR_ENABLED
-        // optionally retract landing gear
-        copter.landinggear.retract_after_takeoff();
-#endif
-    }
 }
 
 // pos_control_run - runs the guided position controller
@@ -401,179 +162,6 @@ void ModeDrawStar::pos_control_run()
 
     // call attitude controller with auto yaw
     attitude_control->input_thrust_vector_heading(pos_control->get_thrust_vector(), auto_yaw.get_heading());
-}
-
-
-// pause_control_run - runs the guided mode pause controller
-// called from guided_run
-void ModeDrawStar::pause_control_run()
-{
-    // if not armed set throttle to zero and exit immediately
-    if (is_disarmed_or_landed()) {
-        // do not spool down tradheli when on the ground with motor interlock enabled
-        make_safe_ground_handling(copter.is_tradheli() && motors->get_interlock());
-        return;
-    }
-
-    // set motors to full range
-    motors->set_desired_spool_state(AP_Motors::DesiredSpoolState::THROTTLE_UNLIMITED);
-
-    // set the horizontal velocity and acceleration targets to zero
-    Vector2f vel_xy, accel_xy;
-    pos_control->input_vel_accel_xy(vel_xy, accel_xy, false);
-
-    // set the vertical velocity and acceleration targets to zero
-    float vel_z = 0.0;
-    pos_control->input_vel_accel_z(vel_z, 0.0, false);
-
-    // call velocity controller which includes z axis controller
-    pos_control->update_xy_controller();
-    pos_control->update_z_controller();
-
-    // call attitude controller
-    attitude_control->input_thrust_vector_rate_heading(pos_control->get_thrust_vector(), 0.0);
-}
-
-// angle_control_run - runs the guided angle controller
-// called from guided_run
-void ModeDrawStar::angle_control_run()
-{
-    float climb_rate_cms = 0.0f;
-    if (!guided_angle_state.use_thrust) {
-        // constrain climb rate
-        climb_rate_cms = constrain_float(guided_angle_state.climb_rate_cms, -wp_nav->get_default_speed_down(), wp_nav->get_default_speed_up());
-
-        // get avoidance adjusted climb rate
-        climb_rate_cms = get_avoidance_adjusted_climbrate(climb_rate_cms);
-    }
-
-    // check for timeout - set lean angles and climb rate to zero if no updates received for 3 seconds
-    uint32_t tnow = millis();
-    if (tnow - guided_angle_state.update_time_ms > get_timeout_ms()) {
-        guided_angle_state.attitude_quat.from_euler(Vector3f(0.0, 0.0, attitude_control->get_att_target_euler_rad().z));
-        guided_angle_state.ang_vel.zero();
-        climb_rate_cms = 0.0f;
-        if (guided_angle_state.use_thrust) {
-            // initialise vertical velocity controller
-            pos_control->init_z_controller();
-            guided_angle_state.use_thrust = false;
-        }
-    }
-
-    // interpret positive climb rate or thrust as triggering take-off
-    const bool positive_thrust_or_climbrate = is_positive(guided_angle_state.use_thrust ? guided_angle_state.thrust : climb_rate_cms);
-    if (motors->armed() && positive_thrust_or_climbrate) {
-        copter.set_auto_armed(true);
-    }
-
-    // if not armed set throttle to zero and exit immediately
-    if (!motors->armed() || !copter.ap.auto_armed || (copter.ap.land_complete && !positive_thrust_or_climbrate)) {
-        // do not spool down tradheli when on the ground with motor interlock enabled
-        make_safe_ground_handling(copter.is_tradheli() && motors->get_interlock());
-        return;
-    }
-
-    // TODO: use get_alt_hold_state
-    // landed with positive desired climb rate, takeoff
-    if (copter.ap.land_complete && (guided_angle_state.climb_rate_cms > 0.0f)) {
-        zero_throttle_and_relax_ac();
-        motors->set_desired_spool_state(AP_Motors::DesiredSpoolState::THROTTLE_UNLIMITED);
-        if (motors->get_spool_state() == AP_Motors::SpoolState::THROTTLE_UNLIMITED) {
-            set_land_complete(false);
-            pos_control->init_z_controller();
-        }
-        return;
-    }
-
-    // set motors to full range
-    motors->set_desired_spool_state(AP_Motors::DesiredSpoolState::THROTTLE_UNLIMITED);
-
-    // call attitude controller
-    if (guided_angle_state.attitude_quat.is_zero()) {
-        attitude_control->input_rate_bf_roll_pitch_yaw(ToDeg(guided_angle_state.ang_vel.x) * 100.0f, ToDeg(guided_angle_state.ang_vel.y) * 100.0f, ToDeg(guided_angle_state.ang_vel.z) * 100.0f);
-    } else {
-        attitude_control->input_quaternion(guided_angle_state.attitude_quat, guided_angle_state.ang_vel);
-    }
-
-    // call position controller
-    if (guided_angle_state.use_thrust) {
-        attitude_control->set_throttle_out(guided_angle_state.thrust, true, copter.g.throttle_filt);
-    } else {
-        pos_control->set_pos_target_z_from_climb_rate_cm(climb_rate_cms);
-        pos_control->update_z_controller();
-    }
-}
-
-
-// returns true if pilot's yaw input should be used to adjust vehicle's heading
-bool ModeDrawStar::use_pilot_yaw(void) const
-{
-    return (copter.g2.guided_options.get() & uint32_t(Options::IgnorePilotYaw)) == 0;
-}
-
-// Guided Limit code
-
-// limit_clear - clear/turn off guided limits
-void ModeDrawStar::limit_clear()
-{
-    guided_limit_1.timeout_ms = 0;
-    guided_limit_1.alt_min_cm = 0.0f;
-    guided_limit_1.alt_max_cm = 0.0f;
-    guided_limit_1.horiz_max_cm = 0.0f;
-}
-
-// limit_set - set guided timeout and movement limits
-void ModeDrawStar::limit_set(uint32_t timeout_ms, float alt_min_cm, float alt_max_cm, float horiz_max_cm)
-{
-    guided_limit_1.timeout_ms = timeout_ms;
-    guided_limit_1.alt_min_cm = alt_min_cm;
-    guided_limit_1.alt_max_cm = alt_max_cm;
-    guided_limit_1.horiz_max_cm = horiz_max_cm;
-}
-
-// limit_init_time_and_pos - initialise guided start time and position as reference for limit checking
-//  only called from AUTO mode's auto_nav_guided_start function
-void ModeDrawStar::limit_init_time_and_pos()
-{
-    // initialise start time
-    guided_limit_1.start_time = AP_HAL::millis();
-
-    // initialise start position from current position
-    guided_limit_1.start_pos = inertial_nav.get_position_neu_cm();
-}
-
-// limit_check - returns true if guided mode has breached a limit
-//  used when guided is invoked from the NAV_GUIDED_ENABLE mission command
-bool ModeDrawStar::limit_check()
-{
-    // check if we have passed the timeout
-    if ((guided_limit_1.timeout_ms > 0) && (millis() - guided_limit_1.start_time >= guided_limit_1.timeout_ms)) {
-        return true;
-    }
-
-    // get current location
-    const Vector3f& curr_pos = inertial_nav.get_position_neu_cm();
-
-    // check if we have gone below min alt
-    if (!is_zero(guided_limit_1.alt_min_cm) && (curr_pos.z < guided_limit_1.alt_min_cm)) {
-        return true;
-    }
-
-    // check if we have gone above max alt
-    if (!is_zero(guided_limit_1.alt_max_cm) && (curr_pos.z > guided_limit_1.alt_max_cm)) {
-        return true;
-    }
-
-    // check if we have gone beyond horizontal limit
-    if (guided_limit_1.horiz_max_cm > 0.0f) {
-        const float horiz_move = get_horizontal_distance_cm(guided_limit_1.start_pos.xy(), curr_pos.xy());
-        if (horiz_move > guided_limit_1.horiz_max_cm) {
-            return true;
-        }
-    }
-
-    // if we got this far we must be within limits
-    return false;
 }
 
 const Vector3p &ModeDrawStar::get_target_pos() const
